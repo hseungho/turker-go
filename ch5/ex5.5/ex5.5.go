728068bfc66fc06d97ebe3675a3649b8d6e7f37d
hseungho <hsh47607@gmail.com>
2023-08-14 00:31:56 +0900

    new: ex5.5 fmt 표준 입력 Scan()
    Signed-off-by: hseungho <hsh47607@gmail.com>

--- ch5/ex5.5/ex5.5.go
+++ ch5/ex5.5/ex5.5.go
@@ -1,1 +1,15 @@
-package ex5_5
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b int
+
+	n, err := fmt.Scan(&a, &b) //
+	if err != nil {
+		fmt.Println(n, err)
+	} else {
+		fmt.Println(n, a, b)
+	}
+}
